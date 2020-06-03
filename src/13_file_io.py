37ec98e93a2e5fb43f550a5826efb6ce37b75f4e
Hunter Stevens <hunterstevens1996@gmail.com>
2020-06-02 18:22:16 -0600

    08_comprehensions and 13_file_io completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+my_text = open('foo.txt','r')
+read_txt = my_text.read()
+my_text.close()
+print(read_txt)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as bar:
+    bar.write("I can code \n you can code \n we can code")
+
+new_txt = open('bar.txt', 'r')
+new_txt_read = new_txt.read()
+new_txt.close()
+print(new_txt_read)
